4fbbf7c5fb09f4fe1e06eba922c3f52048828c7f
Amarthya Ravi <amarthya10@gmail.com>
2022-05-18 12:19:48 +0530

    defaultProps added

--- src/views/pages/Home/fragments/InputSpec.jsx
+++ src/views/pages/Home/fragments/InputSpec.jsx
@@ -1,69 +1,69 @@
 import React from 'react';
-import {Switch } from '@mui/material';
-import InputBox from "../../../shared/components/InputBox";
-
+import { Switch } from '@mui/material';
+import InputBox from '../../../shared/components/InputBox';
 
 const label = { inputProps: { 'aria-label': 'Switch demo' } };
 
 export default function InputSpec() {
-const [scheduleJob, setScheduleJob] = React.useState(false);
+  const [scheduleJob, setScheduleJob] = React.useState(false);
 
-    return (
-        <div className='app'>
-        <h1 className='Title'>Input Specs</h1>
-        <hr/>
-        <form>
-        <label htmlFor='TypeDropDown'>
-            Type
-            <br />
-            <select id='TypeDropDown'>
+  return (
+    <div className="app">
+      <h1 className="Title">Input Specs</h1>
+      <hr />
+      <form>
+        <label htmlFor="TypeDropDown">
+          Type
+          <br />
+          <select id="TypeDropDown">
             <option value="HTTP">HTTP</option>
             <option value="MQTT">MQTT</option>
             <option value="AMQP">AMQP</option>
             <option value="GRPC">GRPC</option>
-            </select>
+          </select>
         </label>
         <br />
-        <InputBox name="URL" id="URLId"/>
-        <label htmlFor='apiType'>
-            Request Type
-            <br />
-            <select id='apiType'>
+        <InputBox name="URL" id="URLId" />
+        <label htmlFor="apiType">
+          Request Type
+          <br />
+          <select id="apiType">
             <option value="GET">GET</option>
             <option value="POST">POST</option>
-            </select>
+          </select>
         </label>
         <br />
         <div className="InputSpec_ScheduleJob">
-            <p >Schedule Job</p>
-            <Switch
+          <p>Schedule Job</p>
+          <Switch
             {...label}
             checked={scheduleJob}
             onChange={(ev, val) => setScheduleJob(val)}
-            />
+          />
         </div>
 
-            {!scheduleJob ? <>
-                <InputBox name="Polling Interval" id="PollingId"/>
-            </> : <>
-                <h1 className='Title'>Minio Config</h1>
-                <hr/>
-                <InputBox name="URL" id="URLId2"/>
-                <InputBox name="Bucket" id="BucketId"/>
-                <InputBox name="State Name" id="StateNameId"/>
-                <InputBox name="Access Key" id="AccessKeyId"/>
-                <InputBox name="Secret Key" id="SecretKeyId"/>
-            </>}
+        {!scheduleJob ? (
+          <InputBox name="Polling Interval" id="PollingId" />
+        ) : (
+          <>
+            <h1 className="Title">Minio Config</h1>
+            <hr />
+            <InputBox name="URL" id="URLId2" />
+            <InputBox name="Bucket" id="BucketId" />
+            <InputBox name="State Name" id="StateNameId" />
+            <InputBox name="Access Key" id="AccessKeyId" />
+            <InputBox name="Secret Key" id="SecretKeyId" />
+          </>
+        )}
 
-            <button className='ColoredButton' onClick={null} type = "button">
-                Run
-            </button>
+        <button className="ColoredButton" onClick={null} type="button">
+          Run
+        </button>
 
-            <button className='BoderedButton'onClick={null} type = "button">
-                Stop Execution
-            </button>
-
-        </form>
+        <button className="BoderedButton" onClick={null} type="button">
+          Stop Execution
+        </button>
+      </form>
     </div>
   );
 }
--- src/views/shared/components/InputBox.jsx
+++ src/views/shared/components/InputBox.jsx
@@ -1,19 +1,23 @@
-import React from "react";
+import React from 'react';
 import PropTypes from 'prop-types';
 
-export default function InputBox({name, id}){
-    return(
+export default function InputBox({ name, id }) {
+  return (
     <div className="inputBox">
-        <label htmlFor={id}>{name}</label>
-        <br/>
-        <input type="text" id={id} name={name}/>
-        <br/>
+      <label htmlFor={id}>{name}</label>
+      <br />
+      <input type="text" id={id} name={name} />
+      <br />
     </div>
-    )
+  );
 }
 
-
 InputBox.propTypes = {
-    name: PropTypes.string,
-    id: PropTypes.string
+  name: PropTypes.string,
+  id: PropTypes.string,
+};
+
+InputBox.defaultProps = {
+  name: PropTypes.string,
+  id: PropTypes.string,
 };
